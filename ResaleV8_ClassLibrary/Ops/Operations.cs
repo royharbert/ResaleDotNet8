1377ec6cd17c5e32f2b92681be003b733f7c138b
Roy Harbert <rharbert981@comcast.net>
2025-11-10 11:05:21 -0500

    251110-3

--- ResaleV8_ClassLibrary/Ops/Operations.cs
+++ ResaleV8_ClassLibrary/Ops/Operations.cs
@@ -1,15 +1,63 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using ResaleV8_ClassLibrary.DatabaseOps;
+using ResaleV8_ClassLibrary.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using System.Reflection;
-using System.Data;
 
 namespace ResaleV8_ClassLibrary.Ops
 {
     public static class Operations
     {
+        public static void RefreshDDList(ComboBox cbo, List<GenericModel> list)
+        {
+            cbo.Items.Clear();
+            foreach (GenericModel item in list)
+            {
+                cbo.Items.Add(item.Data);
+            }
+        }
+
+        public static List<GenericModel> GetDDItems(string tableName)
+        {
+            MySqlConnection con = ConnectToDB.OpenDB();
+            
+            using (con)
+            {
+                string query = $"SELECT id AS ID, item AS Data FROM {tableName} ORDER BY {tableName.Substring(0, tableName.Length - 1)} ASC";
+                var result = con.Query<GenericModel>(query).ToList();
+                return result;
+            }
+        }
+
+        public static int FindStringInList(string searchString, List<GenericModel> list)
+        {
+            foreach (GenericModel item in list)
+            {
+                if (item.Data.Equals(searchString, StringComparison.OrdinalIgnoreCase))
+                {
+                    return item.ID;
+                }
+            }
+            return -1; // Not found
+        }
+        public static string EscapeApostrophes(string input)
+        {
+            if (input.Contains("''"))
+            {
+                if (string.IsNullOrEmpty(input))
+                {
+                    return input;
+                }
+                return input;
+            }
+            return input.Replace("'", "''");
+        }
+
         public static List<string> getModelPtoperties<T>(T model)
         {
             List<string> properties = new List<string>();
@@ -17,22 +65,22 @@ namespace ResaleV8_ClassLibrary.Ops
             foreach (PropertyInfo prop in props)
             {
                 properties.Add(prop.Name);
-            }            
+            }
 
             return properties;
         }
 
-        public static List<string> ConvertDataTableToList(DataTable dt, string columnName)         
+        public static List<string> ConvertDataTableToList(DataTable dt, string columnName)
         {
             List<string> data = new List<string>();
             string item = "";
             if (dt != null)
             {
                 foreach (DataRow row in dt.Rows)
-                {  
+                {
                     item = row[columnName].ToString();
                     data.Add(item);
-                } 
+                }
             }
             return data;
         }
